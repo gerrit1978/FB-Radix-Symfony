9361adaa9b82f7b1d20c5ee897ac58b199eba1c0
Gerrit Vos <gevo27@gmail.com>
2013-11-21 09:48:58 +0100

    Frontend

--- src/Radix/RecruitmentBundle/Controller/FrontendController.php
+++ src/Radix/RecruitmentBundle/Controller/FrontendController.php
@@ -3,11 +3,34 @@
 namespace Radix\RecruitmentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Radix\RecruitmentBundle\Entity\Job;
 
 class FrontendController extends Controller
 {
     public function frontendAction($accountid)
     {
-        return $this->render('RadixRecruitmentBundle:Frontend:frontend.html.twig', array('account' => $accountid));
+    
+      /* We get the config parameters (XML URL/USER/PASS) */
+      $config = $this->getDoctrine()
+        ->getRepository('RadixRecruitmentBundle:Config')
+        ->findBy(array('accountid' => $accountid));
+ 
+      if (!$config) {
+        throw $this->createNotFoundException('No config found for this accountid.');
+      }
+
+      /* We get the jobs */
+      $jobs = $this->getDoctrine()
+        ->getRepository('RadixRecruitmentBundle:Job')
+        ->findBy(array('accountid' => $accountid));
+
+      $jobs_output = array();
+      foreach ($jobs as $job) {
+        $jobs_output[] = array(
+          'title' => $job->getTitle(),
+          'description' => $job->getDescription(),
+        );
+      }      
+      return $this->render('RadixRecruitmentBundle:Frontend:frontend.html.twig', array('jobs' => $jobs_output));
     }
 }
