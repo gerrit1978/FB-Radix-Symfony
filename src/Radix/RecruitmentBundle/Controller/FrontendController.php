3c2d69ee1bc64381527c325946f1f988bca2aecc
Gerrit Vos <gevo27@gmail.com>
2013-11-21 12:28:02 +0100

    Detail

--- src/Radix/RecruitmentBundle/Controller/FrontendController.php
+++ src/Radix/RecruitmentBundle/Controller/FrontendController.php
@@ -29,8 +29,31 @@ class FrontendController extends Controller
         $jobs_output[] = array(
           'title' => $job->getTitle(),
           'description' => $job->getDescription(),
+          'pagelink' => $this->generateUrl('radix_detail', array('accountid' => $accountid, 'guid' => $job->getGuid())),
         );
       }      
       return $this->render('RadixRecruitmentBundle:Frontend:frontend.html.twig', array('jobs' => $jobs_output));
+    }
+    
+    public function detailAction($accountid, $guid) {
+    
+      $config = $this->getDoctrine()
+        ->getRepository('RadixRecruitmentBundle:Config')
+        ->findBy(array('accountid' => $accountid));
+      
+      if (!$config) {
+        throw $this->createNotFoundException('No config found for this accountid.');
+      }
+      
+      // get the job details
+      $job = $this->getDoctrine()
+        ->getRepository('RadixRecruitmentBundle:Job')
+        ->findOneBy(array('guid' => $guid));
+      
+      $job_output = array('title' => $job->getTitle(), 'description' => $job->getDescription());
+      
+      $overview_link = $this->generateUrl('radix_frontend', array('accountid' => $accountid));
+    
+      return $this->render('RadixRecruitmentBundle:Frontend:detail.html.twig', array('job' => $job_output, 'overview_link' => $overview_link));
     }
 }
