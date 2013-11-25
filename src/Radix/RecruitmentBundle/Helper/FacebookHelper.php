0cc31bd9e099ab48a67880238f92f090f4485e0c
Gerrit Vos <gerrit@radix-recruitment.be>
2013-11-25 14:48:39 +0100

    Extended permissions etc

--- src/Radix/RecruitmentBundle/Helper/FacebookHelper.php
+++ src/Radix/RecruitmentBundle/Helper/FacebookHelper.php
@@ -4,18 +4,21 @@ namespace Radix\RecruitmentBundle\Helper;
 
 class FacebookHelper {
 
+  private $facebook;
+
   public function __construct() {
-  }
-  
-  public function isPageAdmin() {
     $config = array(
       'appId' => '600850943303218',
       'secret' => '41938c8ed1d54041769cb346ffac04d2',
     );
 
-    $facebook = new \Facebook($config);
+    $this->facebook = new \Facebook($config);
+
+  }
+  
+  public function isPageAdmin() {
     
-    $signed_request = $facebook->getSignedRequest();
+    $signed_request = $this->facebook->getSignedRequest();
     
     if (isset($signed_request['page']['admin']) && $signed_request['page']['admin'] == 1) {
       $_SESSION['signed_request'] = $signed_request;
@@ -44,8 +47,51 @@ class FacebookHelper {
   }
   
   public function hasAuthorized() {
-    exit('functie aangeroepen');
+    
+    $user_id = $this->facebook->getUser();
+
+    if($user_id) {
+
+      // We have a user ID, so probably a logged in user.
+      // If not, we'll get an exception, which we handle below.
+      try {
+        $ret_obj = $this->facebook->api('/me/feed', 'POST',
+                                    array(
+                                      'link' => 'www.example.com',
+                                      'message' => 'Posting with the PHP SDK!'
+                                 ));
+        echo '<pre>Post ID: ' . $ret_obj['id'] . '</pre>';
+
+        // Give the user a logout link 
+        echo '<br /><a href="' . $this->facebook->getLogoutUrl() . '">logout</a>';
+      } catch(FacebookApiException $e) {
+        // If the user is logged out, you can have a 
+        // user ID even though the access token is invalid.
+        // In this case, we'll get an exception, so we'll
+        // just ask the user to login again here.
+        $login_url = $this->facebook->getLoginUrl( array(
+                       'scope' => 'publish_stream'
+                       )); 
+        echo 'Please <a href="' . $login_url . '">login.</a>';
+        error_log($e->getType());
+        error_log($e->getMessage());
+      }   
+    } else {
+
+      // No user, so print a link for the user to login
+      // To post to a user's wall, we need publish_stream permission
+      // We'll use the current URL as the redirect_uri, so we don't
+      // need to specify it here.
+      $login_url = $this->facebook->getLoginUrl( array( 'scope' => 'publish_stream' ) );
+      //$login_url = "http://www.facebook.com/dialog/oauth?app_id=600850943303218&display=popup&redirect_uri=http://apps.facebook.com/radix-symfony&response_type=token%2Csigned_request&scope=user_birthday%2Cuser_interests%2Cuser_work_history%2Cuser_education_history%2Cuser_location%2Cemail%2Cpublish_stream&sdk=joey";
+      echo 'Please <a target="_top" href="' . $login_url . '">login.</a>';
+
+    } 
+    
+    exit('einde');
+    
   }
+  
   
 }
 
