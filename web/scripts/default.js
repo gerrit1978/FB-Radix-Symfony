864b25e3df4395c0cdd5511b2abbc057ae1ab0e3
Gerrit Vos <gerrit@radix-recruitment.be>
2013-12-13 21:24:59 +0100

    IE support etc

--- web/scripts/default.js
+++ web/scripts/default.js
@@ -1,4 +1,18 @@
-$(document).ready(function() {
+$("document").ready(function() {
+
+  $.ajaxSetup({ cache: true });
+  $.getScript('//connect.facebook.net/en_UK/all.js', function(){
+    // we initialize the facebook object. By adding 'xfbml' we make the "Like" plugin on the frontpage work
+    FB.init({
+      appId: '600850943303218',
+      xfbml: true
+    });
+
+    FB.Canvas.setAutoGrow(); //from 5 July
+    FB.Canvas.scrollTo(0,0);
+  });
+  
+
 
   $('li.job').click(function() {
     var url = $(this).data('job-url');
